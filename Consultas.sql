30b1d14dc16faf543330d82e17773619202165b6
Hfroiman <hernan.froiman@alumnos.frgp.utn.edu.ar>
2022-10-19 16:13:50 -0300

    Resolucion ejercicio

--- Consultas.sql
+++ Consultas.sql
@@ -1,16 +1,16 @@
 -- Los pedidos que hayan sido finalizados en menor cantidad de días que la demora promedio
 
-    SELECT 
-    AVG(pr.DiasConstruccion)
-    from Productos pr
+SELECT 
+AVG(pr.DiasConstruccion)
+from Productos pr
 
 
-     select 
-        *,
-        DATEDIFF(DAY,pe.FechaSolicitud,pe.FechaFinalizacion) Demora
-    from Pedidos pe
-    where DATEDIFF(DAY,pe.FechaSolicitud,pe.FechaFinalizacion) > (SELECT AVG(pr.DiasConstruccion) from Productos pr)
-    order by Demora asc
+select 
+    *,
+    DATEDIFF(DAY,pe.FechaSolicitud,pe.FechaFinalizacion) Demora
+from Pedidos pe
+where DATEDIFF(DAY,pe.FechaSolicitud,pe.FechaFinalizacion) > (SELECT AVG(pr.DiasConstruccion) from Productos pr)
+order by Demora asc
 
 -- Los productos cuyo costo sea mayor que el costo del producto de Roble más caro.
 
@@ -59,9 +59,6 @@ inner JOIN Productos pr on pe.IDProducto=pr.ID
 inner join Materiales_x_Producto mx on pr.ID=mx.IDProducto
 inner join Materiales ma on mx.IDMaterial=ma.ID
 where  year(pe.FechaSolicitud) = 2022 and ma.Nombre='Pino')
-
-
-
 
 
 
@@ -121,29 +118,30 @@ SELECT
 FROM Productos pr
 
 
-
-
-
 -- Por cada producto, la descripción y la cantidad de pedidos enviados y la cantidad de pedidos sin envío.
 
-SELECT
-    pr.Descripcion,
-    (
-        select COUNT(pe.ID) from Envios e
-            inner JOIN Pedidos pe on e.IDPedido=pe.ID
-            where pe.IDProducto=pr.ID
-    )CantEnviados,
-    (
-    /*CALCULAR LOS QUE NO FUERON ENVIADOS*/
-    )CantNOEnviados
-from Productos pr
-
-select COUNT(pe.ID) from Envios e
-inner JOIN Pedidos pe on e.IDPedido=pe.ID
-inner join Productos pr on pe.IDProducto=pr.ID
-
-
 -- Por cada cliente, apellidos y nombres y la cantidad de pedidos solicitados en los años 2020, 2021 y 2022. (Cada año debe mostrarse en una columna separada)
+
+SELECT
+    cl.Apellidos,
+    cl.Nombres,
+    (SELECT
+    count(pe.FechaSolicitud) cant
+    from Pedidos pe
+    where year(pe.FechaSolicitud)= 2020) Año2020
+from Clientes cl
+
+
+
+    (SELECT
+        pe.FechaSolicitud
+        from Pedidos pe
+        where year(pe.FechaSolicitud)= 2021) Año2021,
+    (SELECT
+        pe.FechaSolicitud
+        from Pedidos pe
+        where year(pe.FechaSolicitud)= 2022) Año2022
+
 -- Por cada producto, listar la descripción del producto, el costo y los materiales de construcción (en una celda separados por coma)
 -- Por cada pedido, listar el ID, la fecha de solicitud, el nombre del producto, los apellidos y nombres de los colaboradores que trabajaron en el pedido y la/s tareas que el colaborador haya realizado (en una celda separados por coma)
 -- Las descripciones de los productos que hayan requerido el doble de colaboradores fulltime que colaboradores partime.
